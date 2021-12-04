e65751a55b6fb0409d56b19bae81225ec47bbe6e
Mattias Lasersköld <mattias.l.sk@gmail.com>
2021-12-04 18:01:29 +0100

    Restructure and move some functions

--- include/sdlpp/surface.hpp
+++ include/sdlpp/surface.hpp
@@ -19,7 +19,9 @@ struct SurfaceImpl : Container {
                 Uint32 Rmask,
                 Uint32 Gmask,
                 Uint32 Bmask,
-                Uint32 Amask);
+                Uint32 Amask)
+        : Container{SurfaceImpl::create(
+              flags, width, height, depth, Rmask, Gmask, Bmask, Amask)} {}
 
     inline int lock() {
         return SDL_LockSurface(this->get());
@@ -38,7 +40,48 @@ struct SurfaceImpl : Container {
     }
 
     inline int getColorKey(Uint32 *key) {
-        SDL_GetColorKey(this->get(), key);
+        return SDL_GetColorKey(this->get(), key);
+    }
+
+    static inline SurfaceImpl create(Uint32 flags,
+                                     int width,
+                                     int height,
+                                     int depth,
+                                     Uint32 Rmask,
+                                     Uint32 Gmask,
+                                     Uint32 Bmask,
+                                     Uint32 Amask) {
+        return {SDL_CreateRGBSurface(
+            flags, width, height, depth, Rmask, Gmask, Bmask, Amask)};
+    }
+
+    static inline SurfaceImpl createWithFormat(
+        Uint32 flags, int width, int height, int depth, Uint32 format) {
+        return {SDL_CreateRGBSurfaceWithFormat(
+            flags, width, height, depth, format)};
+    }
+
+    static inline SurfaceImpl createFrom(void *pixels,
+                                         int width,
+                                         int height,
+                                         int depth,
+                                         int pitch,
+                                         Uint32 Rmask,
+                                         Uint32 Gmask,
+                                         Uint32 Bmask,
+                                         Uint32 Amask) {
+        return {SDL_CreateRGBSurfaceFrom(
+            pixels, width, height, depth, pitch, Rmask, Gmask, Bmask, Amask)};
+    }
+
+    static inline SurfaceImpl createWithFormatFrom(void *pixels,
+                                                   int width,
+                                                   int height,
+                                                   int depth,
+                                                   int pitch,
+                                                   Uint32 format) {
+        return {SDL_CreateRGBSurfaceWithFormatFrom(
+            pixels, width, height, depth, pitch, format)};
     }
 };
 
@@ -52,51 +95,5 @@ using SurfaceView = detail::SurfaceImpl<detail::View<SDL_Surface>>;
 inline Surface loadBmp(std::string_view data) {
     return Surface{SDL_LoadBMP(data.data())};
 }
-
-inline Surface createRGBSurface(Uint32 flags,
-                                int width,
-                                int height,
-                                int depth,
-                                Uint32 Rmask,
-                                Uint32 Gmask,
-                                Uint32 Bmask,
-                                Uint32 Amask) {
-    return {SDL_CreateRGBSurface(
-        flags, width, height, depth, Rmask, Gmask, Bmask, Amask)};
-}
-
-inline Surface createRGBSurfaceWithFormat(
-    Uint32 flags, int width, int height, int depth, Uint32 format) {
-    return {
-        SDL_CreateRGBSurfaceWithFormat(flags, width, height, depth, format)};
-}
-
-inline Surface createRGBSurfaceFrom(void *pixels,
-                                    int width,
-                                    int height,
-                                    int depth,
-                                    int pitch,
-                                    Uint32 Rmask,
-                                    Uint32 Gmask,
-                                    Uint32 Bmask,
-                                    Uint32 Amask) {
-    return {SDL_CreateRGBSurfaceFrom(
-        pixels, width, height, depth, pitch, Rmask, Gmask, Bmask, Amask)};
-}
-
-namespace detail {
-
-template <typename Container>
-SurfaceImpl<Container>::SurfaceImpl(Uint32 flags,
-                                    int width,
-                                    int height,
-                                    int depth,
-                                    Uint32 Rmask,
-                                    Uint32 Gmask,
-                                    Uint32 Bmask,
-                                    Uint32 Amask)
-    : Container{createRGBSurface(
-          flags, width, height, depth, Rmask, Gmask, Bmask, Amask)} {}
-} // namespace detail
 
 } // namespace sdl
